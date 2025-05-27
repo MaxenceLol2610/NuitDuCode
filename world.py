d26f2fd6dcacce4a58fe940b03b3c326a222a791
Sirat <wets.mw22@gmail.com>
2025-05-27 12:55:04 +0700

    added map and changed main

--- world.py
+++ world.py
@@ -1,10 +1,23 @@
 import pyxel
+import player
+
+Player = player.PLAYER
+
+def draw_welcome():
+    pyxel.cls(0)
+    pyxel.text(10, 10, "Welcome to the World of Seldha!", 7)
+    pyxel.text(10, 20, "Explore and enjoy your adventure!", 7)
+    pyxel.text(10, 30, "Press 'Q' to quit.", 7)
 
 def draw():
-    world_frame = pyxel.frame_count
-    if world_frame < 30*5:  # Show the welcome message for 5 seconds
-        print("World Frame:", world_frame)
-        pyxel.text(10, 10, "Welcome to the World of Seldha!", 7)
-        pyxel.text(10, 20, "Explore and enjoy your adventure!", 7)
-        pyxel.text(10, 30, "Press 'Q' to quit.", 7)
+    pyxel.cls(9)
+    Player.draw()
+    
+"""
+pyxel.init(256, 256, title="Seldha", fps=30)
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+pyxel.run(update, draw_welcome)
+"""
 
