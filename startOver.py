a5fbcd455dbbb312aaa02a41af1d8f6449208615
M4xyll <maxence.detourniere@gmail.com>
2025-05-27 14:51:07 +0700

    Finish

--- startOver.py
+++ startOver.py
@@ -7,6 +7,7 @@ def welcome():
     pyxel.text(90, 128, "Bienvenue sur Seldha", 11)
     pyxel.text(60, 138, "Appuyez sur <ESPACE> pour commencer", 11)
     pyxel.text(55, 148, "Appuyez sur <ESC> ou <Q> pour quitter", 11)
+    pyxel.text(55, 178, "Appuyez sur <H> pour afficher l'aide", 11)
 
     if pyxel.btn(pyxel.KEY_H) == True:
         help()
@@ -17,8 +18,8 @@ def help():
     pyxel.text(25, 20, "Utilisez les touches du pave directionels pour bouger", 7)
     pyxel.text(25, 35, "Appuyez sur 'J' pour attaquer", 7)
     pyxel.text(25, 50, "Appuyez sur 'U' pour ouvrir le magasin", 7)
-    pyxel.text(25, 60, "Appuyez sur 'H' pour afficher cette aide", 7)
-    pyxel.text(25, 65, "Appuyez sur 'Q' ou 'Echape' pour quitter le jeu", 7)
+    pyxel.text(25, 65, "Appuyez sur 'H' pour afficher cette aide", 7)
+    pyxel.text(25, 80, "Appuyez sur 'Q' ou 'Echape' pour quitter le jeu", 7)
 
 def gameOver():
     pyxel.cls(0)
