47499c5fed58e24f8ec911c8cedb9f1353089b89
M4xyll <maxence.detourniere@gmail.com>
2025-05-27 13:29:13 +0700

    Commit Mob Attacks

--- mobs.py
+++ mobs.py
@@ -16,6 +16,16 @@ class Mobs:
         self.mob_frame = 0
         self.direction = {}
         self.direction_timer = {}
+        self.last_attack = {}
+        self.mage_projectiles = []
+        self.attack_cooldowns = {
+            "skeleton": 45,
+            "mage": 90
+        }
+        self.attack_damages = {
+            "skeleton": 10,
+            "mage": 30
+        }
 
     def mob_nature(self):
         rnd_selector = random.randint(1, 10)
@@ -37,6 +47,7 @@ class Mobs:
             mob_from_player = math.sqrt((self.player_x - x)**2+(self.player_y - y)**2)
             if [x, y, n, h] not in self.ennemis_liste and mob_from_player > mob_exclusion_distance:
                 self.ennemis_liste.append([x, y, n, h])
+
     def mob_movement(self):
         self.player_x, self.player_y = Player.get_position()
         player_health = Player.get_health()
@@ -77,27 +88,77 @@ class Mobs:
                     mob[1] = max(0, min(mob[1], 240))
 
     def ennemis_supression(self):
-        """Removes mobs that are dead"""
         for mob in self.ennemis_liste:
             if mob[3]<= 0:
                 self.ennemis_liste.remove(mob)
-
+    
     def draw(self):
         for mob in self.ennemis_liste:
             if mob[2] == "skeleton":
                 pyxel.blt(mob[0], mob[1], 0, 64, 16, 16, 16, 2, 0, 2)
             elif mob[2] == "mage":
                 pyxel.blt(mob[0], mob[1], 0, 128, 16, 16, 16, 2, 0, 2)
+        
+        for proj in self.mage_projectiles:
+            x, y, _, _, frame = proj
+            u = frame * 16
+            pyxel.blt(x, y, 0, u, 88, 16, 8, 2, 0, 2)
+
     def update(self, frame):
-        """Update the mobs"""
-        # Clean up any removed mobs from direction dictionaries
         current_mobs = {id(mob) for mob in self.ennemis_liste}
         self.direction = {k: v for k, v in self.direction.items() if k in current_mobs}
         self.direction_timer = {k: v for k, v in self.direction_timer.items() if k in current_mobs}
         
         self.enemy_spawn()
         self.mob_movement()
+        self.check_and_perform_attacks(frame)
+        self.update_projectiles()
+        self.ennemis_supression()
         self.mob_frame = frame
+
+    def check_and_perform_attacks(self, frame):
+        for mob in self.ennemis_liste:
+            mob_id = id(mob)
+            x, y, mob_type, health = mob
+            
+            if mob_id not in self.last_attack:
+                self.last_attack[mob_id] = 0
+                
+            if frame - self.last_attack[mob_id] < self.attack_cooldowns[mob_type]:
+                continue
+                
+            dx = self.player_x - x
+            dy = self.player_y - y
+            distance = math.sqrt(dx*dx + dy*dy)
+            
+            if mob_type == "skeleton" and distance <= 16:
+                self.last_attack[mob_id] = frame
+                Player.remove_health(self.attack_damages["skeleton"])
+                
+            elif mob_type == "mage" and distance <= 100:
+                self.last_attack[mob_id] = frame
+                norm = math.sqrt(dx*dx + dy*dy)
+                if norm > 0:
+                    dx = dx/norm * 2
+                    dy = dy/norm * 2
+                    self.mage_projectiles.append([x, y, dx, dy, 0])
+                    
+    def update_projectiles(self):
+        for proj in self.mage_projectiles[:]:
+            x, y, dx, dy, frame = proj
+            proj[0] += dx
+            proj[1] += dy
+            proj[4] = (frame + 1) % 5
+            
+            dx_to_player = self.player_x - x
+            dy_to_player = self.player_y - y
+            if math.sqrt(dx_to_player*dx_to_player + dy_to_player*dy_to_player) < 8:
+                Player.remove_health(self.attack_damages["mage"])
+                self.mage_projectiles.remove(proj)
+                continue
+                
+            if (x < 0 or x > 240 or y < 0 or y > 240):
+                self.mage_projectiles.remove(proj)
 
 MOBS = Mobs()
 
