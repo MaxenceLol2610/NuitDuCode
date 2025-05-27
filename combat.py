a3013e74ae9844aae866a5a4a21f195bed5e5ca5
Alexia <alexiaccode@gmail.com>
2025-05-27 12:38:55 +0700

    meowmeow

--- combat.py
+++ combat.py
@@ -1,3 +1,14 @@
 import pyxel
 import player
 import mobs
+
+
+class combat:
+    def __init__(self):
+        self.player = player.PLAYER
+        self.mobs = mobs.MOBS
+        self.Phealth = self.player.get_health()
+        self.ennemis_liste = self.mobs.ennemis_liste
+    
+    def player_attack(self):
+        pass
