3700a6eb0dcb35194ecae1cc402d75e886360cef
M4xyll <maxence.detourniere@gmail.com>
2025-05-27 09:37:55 +0700

    Start Screen OK v1

--- startOver.py
+++ startOver.py
@@ -2,7 +2,7 @@ import pyxel
 
 def welcome():
     pyxel.cls(0)
-    pyxel.text(50, 50, "Bienvenue sur Seldha", 7)
+    pyxel.text(90, 128, "Bienvenue sur Seldha", 11)
 
 def update():
     if pyxel.btnp(pyxel.KEY_SPACE):
